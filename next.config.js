c4fa04dcfcc3b604ec4fc32c358522021637c7e7
jduarte <jduarte0912@gmail.com>
2020-10-26 16:24:43 -0700

    added pagination

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 const path = require('path')
-const withCSS = require('@zeit/next-css')
 
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
-
-module.exports = withCSS({
-  cssLoaderOptions: {
-    url: false
-  }
-});
